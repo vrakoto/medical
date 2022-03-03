700bc5caf0cfaeb83883f58181cf7ff7c57c0e18
carellien <carellien.rakotoarisoa@gmail.com>
2022-03-03 18:36:18 +0100

    ajout pages

---
+++ pages/ajouterPatient.php
@@ -0,0 +1,36 @@
+<div class="container" id="inscription">
+    <div class="login-box">
+        <div class="login-key">
+            <i class="fa-solid fa-user-injured"></i>
+        </div>
+        <div class="login-title">
+            AJOUTER UN PATIENT
+        </div>
+
+        <div class="login-form" method="POST" action="index.php?page=ajouterPatient">
+            <div class="form-group">
+                <input type="text" id="nom" autocomplete="off" required>
+                <label for="nom"><i class="fa-solid fa-id-card"></i> Nom</label>
+            </div>
+
+            <div class="form-group">
+                <input type="text" id="prenom" autocomplete="off" required>
+                <label for="prenom"><i class="fa-solid fa-id-card"></i> Prénom</label>
+            </div>
+            
+            <div class="form-group">
+                <input type="text" id="age" autocomplete="off" required>
+                <label for="age"><i class="fa-solid fa-calendar-days"></i> Age</label>
+            </div>
+
+            <div class="form-group">
+                <input type="text" id="maladie" autocomplete="off" required>
+                <label for="maladie"><i class="fa-solid fa-virus"></i> Maladie</label>
+            </div>
+
+            <div class="login-button">
+                <button class="btn btn-outline-primary" id="btnInscriptionPatient">CRÉER</button>
+            </div>
+        </div>
+    </div>
+</div>
